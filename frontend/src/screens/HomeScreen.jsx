231955b27f498f9987ef7a135abf05c359047ab1
nowinoa <apt.code14@gmail.com>
2025-07-16 21:18:58 +0100

    adding home page with catalog

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import products from '../products';
+import Product from '../components/Product.jsx';
+import { Col, Row } from 'react-bootstrap';
+
+const HomeScreen = () => {
+  return (
+    <>
+     <h1 className='p-3 text-center'>Latest Products</h1>   
+     <Row className='mx-5'>
+        {products.map(product => (
+            <Col sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+            </Col>
+        ))}
+     </Row>
+    </>
+  )
+}
+
+export default HomeScreen

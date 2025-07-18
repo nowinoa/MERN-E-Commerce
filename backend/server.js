df30971ad49ffd3ebc4ea4c1a8531d3544e32676
nowinoa <apt.code14@gmail.com>
2025-07-18 15:53:26 +0100

    adding .env and environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const products = require("./data/products");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("api runnniiiiing");
@@ -15,4 +17,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
